440b744f9120dd29f83ab0bc6d2f876521837614
Dmitry <bushuevdv@mail.ru>
2025-03-30 11:36:38 -0300

    add graph module

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -42,13 +42,3 @@
 // MMMMMMMMNs:./shmMMh  yMMNds/.:smMMMMMMMM
 // MMMMMMMMMMMMdy+/---``---:+sdMMMMMMMMMMMM
 // MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
-
-pub mod database;
-pub mod error;
-pub mod index;
-pub mod query_handler;
-pub mod sql;
-pub mod witchvm;
-
-#[cfg(feature = "local")]
-pub mod local_data;
